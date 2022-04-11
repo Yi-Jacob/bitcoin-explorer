17039ce1450bfbf6cf75f5ca594275d32f46943c
jacobyi11 <jakeyi00@gmail.com>
2022-04-11 23:49:35 +0000

    added navbar no styling

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
 
 export default function Home(props) {
   return (
     <>
-
+      <Navbar>
+        <div className="container">
+          <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Signed in as: <a href="#login">Mark Otto</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
     </>
   );
 }
