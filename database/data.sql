513792f818e9ddcd4a92f31aa0db0eebce335279
jacobyi11 <jakeyi00@gmail.com>
2022-04-12 18:16:25 +0000

    added backend functionality

--- database/data.sql
+++ database/data.sql
@@ -1,14 +1,9 @@
-insert into "boomarks" (
-  "bookmarkId",
-	"userId",
-	"walletAddress",
-	"data",
-	"bookmarkedAt")
+insert into "users" ("userId", "username","hashedPassword")
 
-values (
-  1,
-  1,
-  '34ioBC4EZUM6W1PoGpp4Z3NgpxChnuP4oz',
-  'abc',
-  02:22:22
-)
+values (1, 'test', 'test'), (2, 'test2', 'test2');
+
+insert into "bookmarks" ("bookmarkId", "userId", "walletAddress", "data", "bookmarkedAt")
+
+values (1, 1, '34ioBC4EZUM6W1PoGpp4Z3NgpxChnuP4oz', '{"a": "aa"}', '2019-03-01 00:00:01-06'), (2, 2, '3Bs39s2zNpoTU6sbUTaKXdaU8PuPXm2koN', '{"b": "bb"}', '2022-03-01 00:00:01-06')
+
+returning *;
--- server/index.js
+++ server/index.js
@@ -2,12 +2,38 @@ require('dotenv/config');
 const express = require('express');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
+const pg = require('pg');
 
 const app = express();
 
 app.use(staticMiddleware);
 
 app.use(errorMiddleware);
+
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/finalProject',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get('/api', (req, res) => {
+  const sql = `
+    select *
+      from "bookmarks"
+  `;
+  db.query(sql)
+    .then(result => {
+      const grade = result.rows;
+      res.status(200).json(grade);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
