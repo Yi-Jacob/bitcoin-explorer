47eebdf7031a3cce3783c44512099f2eb26c50fa
jacobyi11 <jakeyi00@gmail.com>
2022-04-12 00:43:48 +0000

    added navbar

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
 export default function Home(props) {
   return (
     <>
-      <Navbar>
+      <Navbar className='bg-dark black navbar-dark'>
         <div className="container">
-          <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>
-              Signed in as: <a href="#login">Mark Otto</a>
-            </Navbar.Text>
-          </Navbar.Collapse>
+          <Navbar.Brand href="#home" className='orange'>
+            <div className="orange">
+              <i className="fa-brands fa-btc orange" />itcoin Exlorer
+            </div>
+          </Navbar.Brand>
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search for Wallet Address</Button>
+          </Form>
         </div>
       </Navbar>
     </>
