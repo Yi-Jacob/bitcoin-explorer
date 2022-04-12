f81bb70297147ad1931374b230a8e907215792b8
jacobyi11 <jakeyi00@gmail.com>
2022-04-12 17:36:40 +0000

    dummy data for backend

---
+++ database/data.sql
@@ -0,0 +1,14 @@
+insert into "boomarks" (
+  "bookmarkId",
+	"userId",
+	"walletAddress",
+	"data",
+	"bookmarkedAt")
+
+values (
+  1,
+  1,
+  '34ioBC4EZUM6W1PoGpp4Z3NgpxChnuP4oz',
+  'abc',
+  02:22:22
+)
