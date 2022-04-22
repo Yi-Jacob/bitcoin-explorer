1c3469627046d73332ddda68e6e8b61f525b1f6c
jacobyi11 <jakeyi00@gmail.com>
2022-04-22 03:55:55 +0000

    in progress

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
@@ -94,6 +95,32 @@ export default class Home extends React.Component {
             </Form>
           </div>
           <div className="row justify-content-between">
+            <Accordion defaultActiveKey={['0']} alwaysOpen>
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>Accordion Item #1</Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                  est laborum.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>Accordion Item #2</Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                  est laborum.
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
             <Card className='orange-border padding-zero grey-background mb-3 '>
               <Card.Header className='font-titillium-web font-bold home-info-text  justify-content-between'>Difficulty Adjustment</Card.Header>
               <div className="row">
@@ -101,7 +128,7 @@ export default class Home extends React.Component {
                 <Card.Title className='sub-info-text col-md-4'>Current Period Progress: {Number(this.state.difficulty.progressPercent).toFixed(2)}%</Card.Title>
                 <Card.Title className='sub-info-text col-md-4'>Remaining Blocks: {this.state.difficulty.remainingBlocks}</Card.Title>
               </div>
-             </Card>
+            </Card>
 
             <div className="col-md-4" style={{ minWidth: '355' }}>
               <Card className='orange-border padding-zero grey-background mb-3'>
